83f4b606a5c1b6bb4f8f7083a61420a7421e3470
linzhiqiang <1152653890@qq.com>
2022-08-06 01:48:27 +0800

    修改

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,26 @@
 interface R {
-  data?:any,
-  meta?:object
+  data?: any;
+  meta?: object;
 }
 
 export interface searchParams {
-  categories?: string,
-  order?: "desc" | "asc",
-  q?: string,
-  purity?:string,
-  sorting?: string,
-  apiKey?: string,
-  color?: string,
-  topRange?: string,
-  atleast?: string,
-  ratios?: string,
-  page?:number
+  categories?: string;
+  order?: string;
+  q?: string;
+  purity?: string;
+  sorting?: string;
+  apiKey?: string;
+  color?: string;
+  topRange?: string;
+  atleast?: string;
+  ratios?: string;
+  page?: number;
+  resolutions?: string;
 }
 
-export function getData(params: searchParams):Promise<R>;
-export function formatCategory(params: string):string;
-export function formatPurity(params: string):string;
+export function getData(params: searchParams): Promise<R>;
+export function formatCategory(params: object): string;
+export function formatPurity(params: object): string;
 declare namespace searchMeta {
-  export let searchMeta:object
+  export let searchMeta: object;
 }
-
